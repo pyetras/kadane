0516fbdbe9598384111400a9b1436f8c1b55b271
Pietras <p@pyetras.com>
2014-05-24 22:58:10 +0200

    testing

---
+++ test-seq.py
@@ -0,0 +1,27 @@
+import random, sys, subprocess, os
+DEVNULL = open(os.devnull, 'wb')
+random.seed()
+
+wd = os.getcwd()
+d = os.path.dirname(os.path.abspath(__file__))
+
+failed = True
+
+for tests in [[[10, 64, 100][i/3], [10, 64, 100][i%3], random.randrange(1000)] for i in range(9)]:
+  print tests,
+  args = [d + '/seq-orignial'] + tests
+  t1 = subprocess.Popen(map(str, args), stderr=subprocess.PIPE, stdout=DEVNULL)
+  out1 = t1.communicate()[1].strip().split(' Time')[0]
+  
+  args = [os.path.join(wd, sys.argv[1])] + tests
+  t2 = subprocess.Popen(map(str, args), stderr=subprocess.PIPE, stdout=DEVNULL)
+  out2 = t2.communicate()[1].strip().split(' Time')[0]
+  
+  if out1 == out2:
+    print "OK"
+  else:
+    print "FAIL"
+    failed = True
+  
+if failed:
+  sys.exit(1)
